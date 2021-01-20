bb1962c511f806140e4cd64c1a49308c33bd9ab2
Jeyfrin <jeyfrin.castrillon@sofka.com.co>
2021-01-20 15:53:39 -0500

    refactor test simulation touch in screen

--- simulateTouch.py
+++ simulateTouch.py
@@ -1,41 +1,37 @@
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.clock import Clock
-from kivy.input.providers.mouse import MouseMotionEvent 
-from kivy.input.factory import MotionEventFactory
 from kivy.base import EventLoop 
+from functools import partial
 
 from kivy.uix.floatlayout import FloatLayout
 
 
 class testSimulateTouch(App):
     
-    def simulateTouch(self, dt):
-        win=EventLoop.window
-        coord = [win.width/2,  win.height - (win.height/2)]
+    def simulateTouch(self, x, y, dt):
+        
+        coord = [x,  self.win.height - y]
         print(coord)
-        win.dispatch(
+        self.win.dispatch(
             'on_mouse_down',
             coord[0], coord[1],
             'left', {}
         )
-        win.dispatch(
+        self.win.dispatch(
             'on_mouse_up',
             coord[0], coord[1],
             'left', {}
         )
-    
-    def inspect(self, *args):
-        print(args)
-        
-    
+            
     def build(self):
-        print(MotionEventFactory.list())
-        Clock.schedule_interval(self.simulateTouch, 2.)
         self.fl = FloatLayout()
         self.btn=Button(text="simulate Touch", size_hint=[None, None], pos_hint={"center_x":0.5, "center_y":0.5})
-        self.btn.bind(on_touch_down=self.inspect)
         self.fl.add_widget(self.btn)
         return self.fl
+    
+    def on_start(self, *args):
+        self.win=EventLoop.window
+        Clock.schedule_interval(partial(self.simulateTouch, self.win.width /2, self.win.height/2), 2.)
     
 testSimulateTouch().run()
